9c91cad27472cc86df253ce29732f949ac79b69a
linc-mvla <100027020@mvla.net>
2023-04-08 16:56:59 -0700

    chunking generation

--- Assets/Scipt/LevelGenerator.cs
+++ Assets/Scipt/LevelGenerator.cs
@@ -4,38 +4,51 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject[] section;
     public Transform playerPos;
+    public GameObject[] sections;
     [SerializeField] float renderDist = 200;
-    private List<GameObject> createdSections = new List<GameObject>();
-    private Vector3 sectionPos;
+    [SerializeField] Vector2 gridSize;
+    private Dictionary<Vector2Int, GameObject> createdSections = new Dictionary<Vector2Int, GameObject>();
+
+    private Vector2Int renderGridNum;
 
     private void Start()
     {
-        sectionPos = playerPos.position;
+        renderGridNum.x = (int)(renderDist/gridSize.x);
+        renderGridNum.y = (int)(renderDist/gridSize.y);
     }
     // Update is called once per frame
     void Update()
     {
-        if (playerPos.position.z + renderDist > sectionPos.z) {
-            GenerateSection();
-            List<GameObject> removeSections = new List<GameObject>();
-            foreach (GameObject obj in createdSections) {
-                if (Vector3.Distance(obj.transform.position, playerPos.position) > renderDist) {
-                    removeSections.Add(obj);
+        int playerX = (int)(playerPos.position.x/gridSize.x);
+        int playerZ = (int)(playerPos.position.z/gridSize.y);
+        for (int x = -renderGridNum.x; x <= renderGridNum.x; x++) {
+            for (int z = -renderGridNum.y; z <= renderGridNum.y; z++)
+            {
+                Vector2Int cell = new Vector2Int(playerX + x, playerZ + z);
+                if (!createdSections.ContainsKey(cell))
+                {
+                    GenerateSection(cell);
                 }
             }
-            foreach (GameObject obj in removeSections) {
-                createdSections.Remove(obj);
-                Destroy(obj);
+        }
+        List<Vector2Int> removeSections = new List<Vector2Int>();
+        foreach (KeyValuePair<Vector2Int, GameObject> obj in createdSections) {
+            if ((Mathf.Abs(playerX - obj.Key.x) > renderGridNum.x) ||
+                (Mathf.Abs(playerZ - obj.Key.y) > renderGridNum.y)) {
+                removeSections.Add(obj.Key);
+                Destroy(obj.Value);
             }
+        }
+        foreach (Vector2Int obj in removeSections) {
+            createdSections.Remove(obj);
         }
     }
 
-    void GenerateSection() {
-        int secNum = Random.Range(0, section.Length);
-        GameObject o = Instantiate(section[secNum], sectionPos, Quaternion.identity);
-        createdSections.Add(o);
-        sectionPos += new Vector3(0, 0, 50);
+    void GenerateSection(Vector2Int cell) {
+        int secNum = Random.Range(0, sections.Length);
+        Vector3 pos = new Vector3(gridSize.x * cell.x, 0, gridSize.y * cell.y);
+        GameObject o = Instantiate(sections[secNum], pos, Quaternion.identity);
+        createdSections.Add(cell, o);
     }
 }
